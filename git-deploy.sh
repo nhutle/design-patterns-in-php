01f26114c95054d9385130e94d491d40374ad3ca
Nhut Le <leminhnhut08t1@gmail.com>
2020-04-07 21:31:40 +0700

    this is a test

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-read -r -p "Commit message: " desc
+read -r -p 'Commit message: ' desc  # prompt user for commit message
 
 git add .
 
 git add -u
 
-git commit -m $desc
+git commit -m "$desc"
 
 git push origin master
